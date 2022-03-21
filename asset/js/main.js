a94c3d4cd3c3fc5d640d5a5551abc40ba89d10cc
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-21 16:40:30 +0100

    stesura completa esercizio

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,30 +6,50 @@ let difficoltà = 0;
 
 btn.addEventListener ('click', function (){
     let difficoltà = document.getElementById("difficulty").value;
-    console.log (difficoltà)
+    console.log (difficoltà) 
+
+    let container = document.getElementById("grid");
+
+    container.classList.add("d-block")
+
+    container.classList.remove("d-none")
+
+    generatoreBox(difficoltà)
 })
 
-if (difficoltà == 1) {
-    let numeroBox = 100;
-    generatoreBox (numeroBox)
-
-}
 
 
+function generatoreBox (difficoltà) {
+    let numeroBox = 0;
+    
+    if (difficoltà == 1) {
+        numeroBox = 100;
+    } else if (difficoltà == 2) {
+        numeroBox = 81;
+    } else {
+        numeroBox = 49;
+    }
+    
+    grid.innerHTML = ""
 
-
-
-
-function generatoreBox (numeroBox) {
     for (let i = 1; i <= numeroBox; i++) {
         let box = document.createElement("div");
-        box.classList.add("box", "d-flex","text-center","align-items-center","justify-content-center", "fw-bold")
+        box.classList.add("box","d-flex","text-center","align-items-center","justify-content-center", "fw-bold")
         grid.appendChild(box)
         box.innerHTML = i
+
+        if (difficoltà == 1) {
+            box.classList.add("easy")
+        } else if (difficoltà == 2) {
+            box.classList.add("medium")
+        } else {
+            box.classList.add("hard")
+        }
     
-        box.addEventListener ('click', function (){
-            this.classList.add('clicked')
-        })
-    
-    }
+        box.addEventListener ('click', toggle)
+    } 
+}
+
+function toggle () {
+    this.classList.toggle('clicked')
 }
