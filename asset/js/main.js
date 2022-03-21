a497fe46be08c0708d0ec5266bbbae317c1635db
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-21 15:12:21 +0100

    stesura griglia con funzione

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,13 +1,32 @@
 let grid = document.getElementById("grid");
 
-for (let i = 1; i <= 100; i++) {
-    let box = document.createElement("div");
-    box.classList.add("box", "d-flex","text-center","align-items-center","justify-content-center", "fw-bold")
-    grid.appendChild(box)
-    box.innerHTML = i
+let btn = document.getElementById("bottone");
 
-    box.addEventListener ('click', function (){
-        this.classList.add('clicked')
-    })
+btn.addEventListener ('click', function (){
+    console.log(document.getElementById("difficulty").value);
+})
 
+let numeroBox = 100;
+
+generatoreBox (numeroBox)
+
+
+
+
+
+
+
+
+function generatoreBox (numeroBox) {
+    for (let i = 1; i <= numeroBox; i++) {
+        let box = document.createElement("div");
+        box.classList.add("box", "d-flex","text-center","align-items-center","justify-content-center", "fw-bold")
+        grid.appendChild(box)
+        box.innerHTML = i
+    
+        box.addEventListener ('click', function (){
+            this.classList.add('clicked')
+        })
+    
+    }
 }
